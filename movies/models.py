5998b0bccf455d4fb16abdeb3260e929f2b15247
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-10-20 13:28:03 +0900

    Update:리뷰피드백

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Actors(models.Model):
+    first_name      = models.CharField(max_length=25)
+    last_name       = models.CharField(max_length=25)
+    date_of_birth   = models.DateField()
+
+    class Meta:
+        db_table = 'owner'
+
+class Dog(models.Model):
+    name     = models.CharField(max_length=25)
+    age      = models.IntegerField(default=0)
+    owner    = models.ForeignKey('Owner', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'dog'
--- owners/views.py
+++ owners/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+
 
 # Create your views here.
 
@@ -10,15 +10,9 @@ from django.views    import View
 
 from owners.models import Owner, Dog 
 
-class SampleView(View):
+class OwnerView(View):
     def post(self, request): # => CREATE
-        #backend
-        # 1. models.py -> 테이블 구조 만들기(column) => CRUD1
-        # 2. view.py -> 데이터 입력 => CRUD2 ()
-            #2-1. 데이터? -> http 통신-> request.body -> json를 변수에 담기
-            #2-2. 데이터를 가공 => json 형태(딕셔너리)에서 key를 이용해서 value를 찾기
-            #2-3. value를 데이터베이스 저장
-            #2-4. value를 저장할 때는 key값과 table의 column이 일치하는 지 보고 다르면 수정해서 로직짜기
+       
         data     = json.loads(request.body)
         print(data)
         owner    = Owner.objects.create(
